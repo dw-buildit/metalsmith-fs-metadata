08fdf10ae3b64cc7692d30fb050d2ca6410b7fb8
Daniel Wilkinson <daniel.wilkinson@wipro.com>
2019-02-27 14:50:59 +0000

    feat: change behaviour of package to load file contents in to metadata from filesystem

--- test/index.js
+++ test/index.js
@@ -6,85 +6,66 @@ var metadata = require('..');
 
 describe('metalsmith-metadata', function(){
   it('should error for malformed data', function(done){
-    var m = Metalsmith('test/fixtures/malformed').use(metadata({ file: 'data.json' }));
+    var m = Metalsmith('test/fixtures').use(metadata({ file: 'test/fixtures/malformed/data.json' }));
     m.build(function(err){
       assert(err);
-      assert(~err.message.indexOf('malformed data'));
-      assert(!exists('test/fixtures/malformed/build'));
+      assert.equal(err.message, 'Err: SyntaxError: Unexpected token \n in JSON at position 8. File: test/fixtures/malformed/data.json');
       done();
     });
   });
 
   it('should parse JSON', function(done){
-    var m = Metalsmith('test/fixtures/json').use(metadata({ file: 'data.json' }));
+    var m = Metalsmith('test/fixtures').use(metadata({ file: 'test/fixtures/json/data.json' }));
     m.build(function(err){
       if (err) return done(err);
       assert.deepEqual(m.metadata().file, { string: 'string' });
-      assert(!exists('test/fixtures/json/build'));
       done();
     });
   });
 
-  it('should not parse a file if the key already exists in the metadata', function(done){
-    var m = Metalsmith('test/fixtures/json')
-      .use(metadata({ file: 'data.json' }))
-      .use(metadata({ file: 'missing.json' }));
+  it('should parse a multiple files for the same key overwriting earlier loaded variables', function(done){
+    var m = Metalsmith('test/fixtures')
+      .use(metadata({ file: 'test/fixtures/duplicate/data.json' }))
+      .use(metadata({ file: 'test/fixtures/duplicate/data2.json' }));
     m.build(function(err){
       if (err) return done(err);
-      assert.deepEqual(m.metadata().file, { string: 'string' });
-      assert(!exists('test/fixtures/json/build'));
-      done();
-    });
-  });
-
-  it('should parse a file even if the key exists if the file is in the bundle', function(done){
-    var m = Metalsmith('test/fixtures/duplicate')
-      .use(metadata({ file: 'data.json' }))
-      .use(metadata({ file: 'data2.json' }));
-    m.build(function(err){
-      if (err) return done(err);
-      assert.deepEqual(m.metadata().file, { string: 'string2' });
-      assert(!exists('test/fixtures/json/build'));
+      assert.deepEqual(m.metadata().file, { string: 'string2', data: true, data2: true });
       done();
     });
   });
 
   it('should parse YAML', function(done){
-    var m = Metalsmith('test/fixtures/yaml').use(metadata({ file: 'data.yaml' }));
+    var m = Metalsmith('test/fixtures').use(metadata({ file: 'test/fixtures/yaml/data.yaml' }));
     m.build(function(err){
       if (err) return done(err);
       assert.deepEqual(m.metadata().file, { string: 'string' });
-      assert(!exists('test/fixtures/yaml/build'));
       done();
     });
   });
 
   it('should parse nested path', function(done){
-    var m = Metalsmith('test/fixtures/nested').use(metadata({ file: 'path/data.yaml' }));
+    var m = Metalsmith('test/fixtures').use(metadata({ file: 'test/fixtures/nested/path/data.yaml' }));
     m.build(function(err){
       if (err) return done(err);
       assert.deepEqual(m.metadata().file, { string: 'string' });
-      assert(!exists('test/fixtures/nested/build'));
       done();
     });
   });
 
   it('should parse nested path with backslash', function(done){
-    var m = Metalsmith('test/fixtures/nested').use(metadata({ file: 'path\\data.yaml' }));
+    var m = Metalsmith('test/fixtures').use(metadata({ file: 'test/fixtures/nested/path\\data.yaml' }));
     m.build(function(err){
       if (err) return done(err);
       assert.deepEqual(m.metadata().file, { string: 'string' });
-      assert(!exists('test/fixtures/nested/build'));
       done();
     });
   });
 
   it('should parse deep nested path', function(done){
-    var m = Metalsmith('test/fixtures/deep-nested').use(metadata({ file: 'path/path/data.yaml' }));
+    var m = Metalsmith('test/fixtures').use(metadata({ file: 'test/fixtures/deep-nested/path/path/data.yaml' }));
     m.build(function(err){
       if (err) return done(err);
       assert.deepEqual(m.metadata().file, { string: 'string' });
-      assert(!exists('test/fixtures/deep-nested/build'));
       done();
     });
   });
